b518807ae4677059bf7969ab5cbd4a0af06be37d
YASSER-TAIBI <yasser.taibi.19@gmail.com>
2025-11-06 13:25:45 +0100

    Implementation du gestionnaire d'exception global

---
+++ banking/src/main/java/com/yazzer/banking/handlers/GlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.yazzer.banking.handlers;
+
+import com.yazzer.banking.exceptions.ObjectValidationException;
+import com.yazzer.banking.exceptions.OperationNonPermittedException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ObjectValidationException.class)
+    public ResponseEntity<ExceptionRepresentation> handleException(ObjectValidationException exeption) {
+        ExceptionRepresentation representation = ExceptionRepresentation.builder()
+                .errorMessage("Object not valid exception has occured")
+                .errorSource(exeption.getViolationSource())
+                .validationErrors(exeption.getViolations())
+                .build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(representation);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ExceptionRepresentation> handleException(EntityNotFoundException exeption) {
+        ExceptionRepresentation representation = ExceptionRepresentation.builder()
+                .errorMessage(exeption.getMessage())
+                .build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(representation);
+    }
+
+    @ExceptionHandler(OperationNonPermittedException.class)
+    public ResponseEntity<ExceptionRepresentation> handleException(OperationNonPermittedException exeption) {
+        ExceptionRepresentation representation = ExceptionRepresentation.builder()
+                .errorMessage(exeption.getMessage())
+                .build();
+        return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(representation);
+    }
+}
