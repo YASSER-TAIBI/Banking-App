e3cce4846f933982fa6f689b649b391b6c1dc99c
YASSER-TAIBI <yasser.taibi.19@gmail.com>
2025-11-17 18:48:50 +0100

    Interface graphique Transactions

--- banking-ui/src/app/app.component.ts
+++ banking-ui/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import {MenuComponent} from './components/menu/menu.component';
 import {UserDashboardComponent} from './pages/user-dashboard/user-dashboard.component';
+import {TransactionsComponent} from './pages/transactions/transactions.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatSlideToggleModule, UserDashboardComponent, MenuComponent],
+  imports: [RouterOutlet, MatSlideToggleModule, UserDashboardComponent, MenuComponent, TransactionsComponent],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.scss'
---
+++ banking-ui/src/app/pages/transactions/transactions.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+
+interface Transaction {
+  date: string;
+  amount: string;
+  iban: string;
+  type: 'Transfer' | 'Deposit';
+}
+
+@Component({
+  selector: 'app-transactions',
+  imports: [
+    MatButtonModule, 
+    MatTableModule,
+    MatIconModule
+  ],
+  templateUrl: './transactions.component.html',
+  standalone: true,
+  styleUrl: './transactions.component.scss'
+})
+export class TransactionsComponent {
+
+  displayedColumns: string[] = ['date', 'amount', 'iban', 'type'];
+
+  transactions: Transaction[] = [
+    { date: '01/01/2023', amount: '1 560€', iban: 'DE12 1234 3456 3456 3455 00', type: 'Transfer' },
+    { date: '01/01/2023', amount: '1 560€', iban: 'DE12 1234 3456 3456 3455 00', type: 'Deposit' },
+    { date: '01/01/2023', amount: '1 560€', iban: 'DE12 1234 3456 3456 3455 00', type: 'Transfer' },
+    { date: '01/01/2023', amount: '1 560€', iban: 'DE12 1234 3456 3456 3455 00', type: 'Transfer' },
+    { date: '01/01/2023', amount: '1 560€', iban: 'DE12 1234 3456 3456 3455 00', type: 'Deposit' },
+    { date: '01/01/2023', amount: '1 560€', iban: 'DE12 1234 3456 3456 3455 00', type: 'Transfer' },
+    { date: '01/01/2023', amount: '1 560€', iban: 'DE12 1234 3456 3456 3455 00', type: 'Deposit' },
+    { date: '01/01/2023', amount: '1 560€', iban: 'DE12 1234 3456 3456 3455 00', type: 'Transfer' },
+    { date: '01/01/2023', amount: '1 560€', iban: 'DE12 1234 3456 3456 3455 00', type: 'Transfer' }
+  ];
+
+  dataSource = new MatTableDataSource<Transaction>(this.transactions);
+
+  onNewTransaction() {
+    // tu ajouteras plus tard l’ouverture d’un dialog ou la navigation vers une page de création
+    console.log('New transaction clicked');
+  }
+}
