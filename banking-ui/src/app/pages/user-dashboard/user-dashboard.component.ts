4ce1dd0187729de23c36c5af0ebedf613dabea22
YASSER-TAIBI <yasser.taibi.19@gmail.com>
2025-11-17 13:34:37 +0100

    Dashboard user UI

--- banking-ui/src/app/pages/user-dashboard/user-dashboard.component.ts
+++ banking-ui/src/app/pages/user-dashboard/user-dashboard.component.ts
@@ -4,20 +4,33 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatListModule} from '@angular/material/list';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-dashboard',
   imports: [
+    FormsModule,
     MatSidenavModule,
     MatIconModule,
     MatButtonModule,
     MatListModule,
-    MatCard
+    MatCard,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   templateUrl: './user-dashboard.component.html',
   standalone: true,
   styleUrl: './user-dashboard.component.scss'
 })
 export class UserDashboardComponent {
+
+  startDate = new Date(2023, 0, 1);
+  endDate = new Date(2023, 0, 1);
 
 }
