7121715ff5002e32946b2da3505c8a9f8d6bb582
YASSER-TAIBI <yasser.taibi.19@gmail.com>
2025-11-18 13:14:58 +0100

    Interface graphique nouveau Contact

--- banking-ui/src/app/pages/new-contact/new-contact.component.ts
+++ banking-ui/src/app/pages/new-contact/new-contact.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-new-contact',
-  imports: [],
+  imports: [
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatButtonModule, 
+    MatIconModule],
   templateUrl: './new-contact.component.html',
+  standalone: true,
   styleUrl: './new-contact.component.scss'
 })
 export class NewContactComponent {
